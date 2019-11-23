66d09543f1b6fe6fc67fadeb85d4cd597bc2a525
Larysa <Larysa76@gmail.com>
2019-11-23 08:20:57 +0200

    Updates

---
+++ precache-manifest.cf852e245db92c1adeaffcf062f3a73b.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0316e63db678007da6527c41fe5bc556",
+    "url": "/SkrypkinaCV/index.html"
+  },
+  {
+    "revision": "bb643134a19c2fc77ff1",
+    "url": "/SkrypkinaCV/static/css/main.b908b991.chunk.css"
+  },
+  {
+    "revision": "5cb72f64a314f73eb81c",
+    "url": "/SkrypkinaCV/static/js/2.8178e709.chunk.js"
+  },
+  {
+    "revision": "bb643134a19c2fc77ff1",
+    "url": "/SkrypkinaCV/static/js/main.535b066c.chunk.js"
+  },
+  {
+    "revision": "d4bacf45382e2c95c88b",
+    "url": "/SkrypkinaCV/static/js/runtime-main.07b5a49b.js"
+  },
+  {
+    "revision": "91c00e19e35b73484c1cb4fe32a9eb64",
+    "url": "/SkrypkinaCV/static/media/Larysa_Skrypkina_-_Junior_Front_end_Developer.91c00e19.pdf"
+  },
+  {
+    "revision": "e90f83814032e0ad9b64069f1a1eec43",
+    "url": "/SkrypkinaCV/static/media/firstimage.e90f8381.jpg"
+  },
+  {
+    "revision": "3a4c6a9baed6b1dac783628dc4f0409e",
+    "url": "/SkrypkinaCV/static/media/secondimage.3a4c6a9b.jpg"
+  },
+  {
+    "revision": "3f3445b008e5d8fa74c48499ec0fbd2c",
+    "url": "/SkrypkinaCV/static/media/thirdimage.3f3445b0.jpg"
+  }
+]);
