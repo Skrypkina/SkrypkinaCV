a660dd8e984d2c1cb3f59c25fc949228b29a5f9b
Larysa <Larysa76@gmail.com>
2019-11-24 21:52:17 +0200

    changed pageTitle

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
+import DocumentTitle from 'react-document-title';
 import Dashboard from '../Dashboard/Dashboard';
 import About from '../About/About';
 import Projects from '../Projects/Projects';
@@ -8,6 +9,9 @@ import Contacts from '../Contacts/Contacts';
 const App = () => {
   return (
     <div>
+      <DocumentTitle title="Home">
+        <h1>Resume</h1>
+      </DocumentTitle>
       <Dashboard />
       <Switch>
         <Route exact path="/" component={About} />
